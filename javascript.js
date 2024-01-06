489cd185d2eba8f6e8fc0d4de2e052140c706b36
ABDELKADER199 <apdoarek22222@gmail.com>
2024-01-07 01:30:40 +0200

    first commit

---
+++ javascript.js
@@ -0,0 +1,39 @@
+let dayH1 = document.querySelector('.day');
+let body = document.querySelector('.body');
+let audio = document.querySelector('audio');
+let prayer = document.querySelector('.prayer');
+// show time 
+
+setInterval(() => {
+    let date = new Date();
+    today = date.getHours()+' : '+date.getMinutes()+' : '+date.getSeconds();
+    dayH1.innerHTML = today;
+
+// change background img by time
+
+if (date.getHours() === 1 && date.getMinutes() === 30) {
+    body.style.backgroundColor = 'red';
+    body.style.backgroundImage = 'url(./img/1838407_0.jpg)';
+    prayer.innerHTML = 'صلاة العصر';
+    body.style.color = 'black'
+    audio.play();
+} else if (date.getHours() === 1 && date.getMinutes() === 31) {
+    body.style.backgroundColor = 'green';
+    body.style.backgroundImage = 'url(./img/azan1.jpg)';
+    prayer.innerHTML = 'صلاة المغرب';
+    body.style.color = 'gold'
+    audio.play();
+} else if (date.getHours() === 1 && date.getMinutes() === 32) {
+    body.style.backgroundColor = 'gold';
+    body.style.backgroundImage = 'url(./img/download.jpg)';
+    prayer.innerHTML = 'صلاة العشاء';
+    body.style.color = 'white'
+    audio.play();
+}else{
+    body.backgroundColor = 'white';
+}
+
+
+})
+
+
