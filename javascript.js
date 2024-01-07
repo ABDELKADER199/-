ee3ead22a53263ce45f82a85c2d25e5b804d7706
ABDELKADER199 <apdoarek22222@gmail.com>
2024-01-07 02:00:47 +0200

    v2

--- javascript.js
+++ javascript.js
@@ -10,20 +10,33 @@ setInterval(() => {
     dayH1.innerHTML = today;
 
 // change background img by time
-
-if (date.getHours() === 1 && date.getMinutes() === 55) {
+ if (date.getHours() === 5 && date.getMinutes() === 20) {
+    body.style.backgroundColor = 'red';
+    body.style.backgroundImage = 'url(./img/1838407_0.jpg)';
+    prayer.innerHTML = 'صلاة الفجر';
+    body.style.color = 'black'
+    audio.play();
+}
+else if (date.getHours() === 12 && date.getMinutes() === 0) {
+    body.style.backgroundColor = 'red';
+    body.style.backgroundImage = 'url(./img/1838407_0.jpg)';
+    prayer.innerHTML = 'صلاة الظهر';
+    body.style.color = 'black'
+    audio.play();
+}
+else if (date.getHours() === 14 && date.getMinutes() === 51) {
     body.style.backgroundColor = 'red';
     body.style.backgroundImage = 'url(./img/1838407_0.jpg)';
     prayer.innerHTML = 'صلاة العصر';
     body.style.color = 'black'
     audio.play();
-} else if (date.getHours() === 1 && date.getMinutes() === 56) {
+} else if (date.getHours() === 17 && date.getMinutes() === 11) {
     body.style.backgroundColor = 'green';
     body.style.backgroundImage = 'url(./img/azan1.jpg)';
     prayer.innerHTML = 'صلاة المغرب';
     body.style.color = 'gold'
     audio.play();
-} else if (date.getHours() === 1 && date.getMinutes() === 57) {
+} else if (date.getHours() === 18 && date.getMinutes() === 32) {
     body.style.backgroundColor = 'gold';
     body.style.backgroundImage = 'url(./img/download.jpg)';
     prayer.innerHTML = 'صلاة العشاء';
